1f0ef0cc547666b1cf9aa84a4509cebb3a366bc1
Vinbu <vincent.14.damian@gmail.com>
2024-05-01 18:14:53 -0400

    Descomposicion en valores
    Descompone el numero "n" en todas sus combinaciones posibles

--- Descomposicion.java
+++ Descomposicion.java
@@ -1,26 +1,23 @@
 import java.util.Scanner;
 public class Descomposicion {
-    static String descomposicion(int n, String unidad){
-        if(n==1){
-            return (unidad);
+    static void descomposicion(int n, String combinacion, int ultimo) {
+        if (n == 0) {
+            System.out.println(combinacion.substring(1)); // Imprimir la combinación sin el primer "+"
+            return;
         }
-        else{
-            if(n!=1){
-                System.out.println((n-1)+"+"+unidad);
-            }
-            return(unidad+"+"+(descomposicion(n-1,unidad)));
+
+        for (int i = 1; i <= Math.min(n, ultimo); i++) {
+            descomposicion(n - i, combinacion + "+" + i, i);
         }
     }
+
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
         System.out.println("Ingrese un número entero n: ");
-
         int n = teclado.nextInt();
-        String unidad="1";
 
-        System.out.println(n+" se puede descomponer como: ");
-        System.out.println(descomposicion(n,unidad));
-
+        System.out.println(n + " se puede descomponer como: ");
+        descomposicion(n, "", n);
     }
 }
